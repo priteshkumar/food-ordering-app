6df62bfe6c992843c85a20c2c8b577625f4eada3
Pritesh Kumar <pritesh.kumar2@gmail.com>
2021-01-11 18:15:16 +0530

    add restaurant details fetch from backend

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -29,8 +29,9 @@ const styles = (theme) => ({
   img: {
     margin: "0 auto",
     display: "block",
-    maxWidth: "90%",
-    maxHeight: "96%",
+    maxWidth: "94%",
+    maxHeight: "100%",
+    height:"280px",
   },
   restaurantInfo: {
     [theme.breakpoints.down("sm")]: {
@@ -52,109 +53,143 @@ const styles = (theme) => ({
 });
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { restaurant: null };
+  }
+
+  componentDidMount() {
+    let that = this;
+    let dataRestaurant = null;
+    let xhrMovie = new XMLHttpRequest();
+    xhrMovie.addEventListener("readystatechange", function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let restaurantInfo = JSON.parse(this.responseText);
+        console.log(restaurantInfo);
+        that.setState({ restaurant: JSON.parse(this.responseText) });
+        console.log(that.state.restaurant);
+      }
+    });
+
+    xhrMovie.open(
+      "GET",
+      this.props.baseUrl + "/restaurant/" + this.props.match.params.id
+    );
+    xhrMovie.setRequestHeader("Cache-Control", "no-cache");
+    xhrMovie.send(dataRestaurant);
+  }
+
   render() {
     const { classes } = this.props;
-    const ratingMsg = "AVERAGE RATING BY\n658 CUSTOMERS";
     const priceMsg = "AVERAGE COST FOR\n TWO PEOPLE";
+    const restaurant = this.state.restaurant;
 
     return (
       <div className={classes.root}>
         <Header baseUrl={this.baseUrl} showSearchBox="false" />
-        <div className={classes.paper}>
-          <Grid
-            container
-            spacing={6}
-            direction="row"
-            alignItems="flex-start"
-            justify="flex-start"
-          >
-            <Grid item xs={12} sm={5} md={4} lg={4}>
-              <div className={classes.image}>
-                <img
-                  className={classes.img}
-                  alt="complex"
-                  src="https://b.zmtcdn.com/data/reviews_photos/94a/be67cc20a6ab663f95330e5af6afb94a_1521359398.jpg"
-                />
-              </div>
-            </Grid>
+        {this.state.restaurant !== null && (
+          <div className={classes.paper}>
             <Grid
-              item
-              xs={12}
-              sm={6}
-              md={7}
-              lg={7}
               container
-              direction="column"
-              spacing={2}
+              spacing={6}
+              direction="row"
+              alignItems="flex-start"
               justify="flex-start"
-             className={classes.restaurantInfo}
             >
-              <Grid item xs>
-                <Typography gutterBottom variant="h3">
-                  Loud Silence
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                  CBD BELAPUR
-                </Typography>
-                <Typography
-                  variant="h6"
-                  color="textPrimary"
-                  style={{ fontWeight: "400" }}
-                  gutterBottom
-                >
-                  Chinese, Continental, Indian, Italian, Snacks
-                </Typography>
+              <Grid item xs={12} sm={5} md={4} lg={4}>
+                <div className={classes.image}>
+                  <img
+                    className={classes.img}
+                    alt="complex"
+                    src={restaurant.photo_URL}
+                  />
+                </div>
               </Grid>
-              <br />
-              <Grid item container direction="row" justify="space-between">
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={7}
+                lg={7}
+                container
+                direction="column"
+                spacing={2}
+                justify="flex-start"
+                className={classes.restaurantInfo}
+              >
                 <Grid item xs>
-                  <Typography variant="subtitle1">
-                    <span style={{ fontSize: "1.4rem" }}>
-                      <Icon
-                        className="fa fa-star"
-                        style={{ marginBottom: "-3px" }}
-                      />{" "}
-                      4.4
-                    </span>
-                    <br />
-                    <span
-                      style={{
-                        whiteSpace: "pre-line",
-                        color: "grey",
-                        fontWeight: "430",
-                        fontSize: "1.2rem",
-                      }}
-                    >
-                      {ratingMsg}
-                    </span>
+                  <Typography gutterBottom variant="h3">
+                    {restaurant.restaurant_name}
+                  </Typography>
+                  <Typography variant="h5" gutterBottom>
+                    {restaurant.address.locality}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    color="textPrimary"
+                    style={{ fontWeight: "400" }}
+                    gutterBottom
+                  >
+                    {restaurant.categories
+                      .map((category) => {
+                        return category.category_name;
+                      })
+                      .join(", ")}
                   </Typography>
                 </Grid>
-                <Grid item xs>
-                  <Typography variant="subtitle1">
-                    <span style={{ fontSize: "1.4rem" }}>
-                      <Icon
-                        className="fa fa-inr"
-                        style={{ marginBottom: "-3px" }}
-                      />
-                      600
-                    </span>
-                    <br />
-                    <span
-                      style={{
-                        whiteSpace: "pre-line",
-                        color: "grey",
-                        fontWeight: "430",
-                        fontSize: "1.2rem",
-                      }}
-                    >
-                      {priceMsg}
-                    </span>
-                  </Typography>
+                <br />
+                <Grid item container direction="row" justify="space-between">
+                  <Grid item xs>
+                    <Typography variant="subtitle1">
+                      <span style={{ fontSize: "1.4rem" }}>
+                        <Icon
+                          className="fa fa-star"
+                          style={{ marginBottom: "-3px" }}
+                        />{" "}
+                        {restaurant.customer_rating}
+                      </span>
+                      <br />
+                      <span
+                        style={{
+                          whiteSpace: "pre-line",
+                          color: "grey",
+                          fontWeight: "430",
+                          fontSize: "1.2rem",
+                        }}
+                      >
+                        {"AVERAGE RATING BY\n" +
+                          restaurant.number_customers_rated +
+                          " CUSTOMERS"}
+                      </span>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs>
+                    <Typography variant="subtitle1">
+                      <span style={{ fontSize: "1.4rem" }}>
+                        <Icon
+                          className="fa fa-inr"
+                          style={{ marginBottom: "-3px" }}
+                        />
+                        {restaurant.average_price}
+                      </span>
+                      <br />
+                      <span
+                        style={{
+                          whiteSpace: "pre-line",
+                          color: "grey",
+                          fontWeight: "430",
+                          fontSize: "1.2rem",
+                        }}
+                      >
+                        {priceMsg}
+                      </span>
+                    </Typography>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </div>
+          </div>
+        )}
       </div>
     );
   }
